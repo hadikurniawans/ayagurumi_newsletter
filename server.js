69c211a31c6e5b36a3a7eb576e742119937a3862
hadikurniawans <hadikaes96@gmail.com>
2018-07-26 19:14:46 +0700

    mailchimp api integration

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const morgan = require('morgan');
 const request = require('request');
 const async = require('async');
 const expressHbs = require('express-handlebars');
+const session = require('express-session');
+const mongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
+
 
 const app = express();
 
@@ -14,14 +18,46 @@ app.use(morgan('dev'));
 app.engine('.hbs', expressHbs({ defaultLayout : 'layout', extname:'.hbs'}));
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+app.use(session({
+	resave: true,
+	saveUninitialized: true,
+	secret: 'ayaguruminewsletterv1',
+	store: new mongoStore({ url: 'mongodb://hadikurniawans:workhard96@ds153851.mlab.com:53851/ayagurumi_newsletter'})
+}));
+
+app.use(flash());
 
 app.route('/')
 	.get((req,res,next) => {
-		res.render('main/home');
+		res.render('main/home', { message: req.flash("success")});
 	})
 
 	.post((req, res, next) => {
-		
+		// console.log(req.body.email);
+		request({
+			url: 'https://us18.api.mailchimp.com/3.0/lists/f7dbc24318/members',
+			method: 'POST',
+			headers:{
+				'Authorization' : 'randomUser ba849bea701017904b9c4bc5cd01e6a9-us18',
+				'Content-Type' : 'application/json'
+
+			},
+			json:{
+				'email_address' : req.body.email,
+				'status' : 'subscribed'
+
+			}
+
+		}, function(err, response, body){
+			if (err){
+				console.log(err);
+			}else{
+				req.flash('success', 'you have submitted your email');
+				res.redirect('/');
+			}
+
+		})
+
 	})
 
 app.listen(3030, (err) => {
