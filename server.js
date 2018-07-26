505b1a3c5c5eb4f5b508cc8b73c2230702e89688
hadikurniawans <hadikaes96@gmail.com>
2018-07-26 17:36:20 +0700

    add server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const request = require('request');
+const async = require('async');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(morgan('dev'));
+
+app.engine('.hbs', expressHbs({ defaultLayout : 'layout', extname:'.hbs'}));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.route('/')
+	.get((req,res,next) => {
+		res.render('main/home');
+	})
+
+	.post((req, res, next) => {
+
+	})
+
+app.listen(3030, (err) => {
+	if (err){
+		console.log(err);
+	}else{
+		console.log('listening port 3030');
+	}
+});
